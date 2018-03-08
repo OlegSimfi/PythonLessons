900ab609cc3b79f71f7ffe3b740aee4f69365dcd
OlegSimfi <olegsimfi@gmail.com>
2018-03-08 10:12:27 +0200

    Add conditionOperators.py

---
+++ conditionalOperators.py
@@ -0,0 +1,17 @@
+# name = input("Enter Name")
+# if name=="Oleg":
+#     print("True")
+
+#
+# num = int(input("Введите число"))
+# if num > 0:
+#     print("True")
+# elif num < 0:
+#     print("Вы ввели число меньше 0")
+# else:
+#     print("Вы ввели 0")
+
+name = input("Введите имя")
+a="Yes" if name != "Test" else "No"
+print(a)
+
