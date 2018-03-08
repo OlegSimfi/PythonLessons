97be50ad1151ddc27821f611e8b53afac468b047
OlegSimfi <olegsimfi@gmail.com>
2018-03-08 14:45:18 +0200

    Add dict.py

---
+++ dict.py
@@ -0,0 +1,21 @@
+d = {'id-1': 1, 'id-2': 'Test'}
+print(d['id-1'])
+print(d['id-2'])
+
+a = dict(short='dict', long='dictionary')
+print(a)
+a['short'] = 234
+print(a)
+
+b = dict.fromkeys(['a','b'])
+print(b)
+b['a'] = 7
+b['b'] = 88
+print(b)
+
+c = {a: a**2 for a in range(7)}
+print(c)
+
+person = {'name': {'last_name': 'Иванов', 'first_name': 'Федор', 'middle_name': 'Иванович'}, 'address': ['г. Киев', 'ул. Хрещатик д. 1', 'кв.17'], 'phone': {'home_phone': '77-67-12', 'mobile_phone': '+380-50-777-77-77', 'mobile_phone_2': 'Нет'}}
+print(person['name']['last_name'])
+print(person['phone']['mobile_phone'])
