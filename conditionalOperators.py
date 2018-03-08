27cab346efe479804dfcecfd6dcd7ddabb727753
OlegSimfi <olegsimfi@gmail.com>
2018-03-08 10:13:48 +0200

    conditionOperators.py FIX

--- conditionalOperators.py
+++ conditionalOperators.py
@@ -1,15 +1,15 @@
-# name = input("Enter Name")
-# if name=="Oleg":
-#     print("True")
+name = input("Enter Name")
+if name=="Oleg":
+    print("True")
 
-#
-# num = int(input("Введите число"))
-# if num > 0:
-#     print("True")
-# elif num < 0:
-#     print("Вы ввели число меньше 0")
-# else:
-#     print("Вы ввели 0")
+
+num = int(input("Введите число"))
+if num > 0:
+    print("True")
+elif num < 0:
+    print("Вы ввели число меньше 0")
+else:
+    print("Вы ввели 0")
 
 name = input("Введите имя")
 a="Yes" if name != "Test" else "No"
