fd5cfd1cf88b6b12f9926a10d58e7ba8aca53988
Oleh Komuniar <olegsimfi@gmail.com>
2019-12-17 13:40:48 +0200

    Add tryExcept

--- functions.py
+++ functions.py
@@ -1,9 +1,44 @@
 def func(x, a):
-    res = x+a
+    res = x + a
     return res
+
+
 print(func(27, 77))
 
 
 def func(*args):
     return args
-print(func(1,2,3))
+
+
+print(func(1, 2, 3))
+
+
+def func(x):
+    def add(a):
+        return x + a
+
+    return add
+
+
+test = func(100)
+print(test(200))
+print(test)
+
+
+def func(r, w, y=2):
+    res = r + w
+    res *= y
+    return res
+
+
+print(func(2, 4))
+
+
+def func(*args):
+    return args
+
+
+print(func(2, 4, 3))
+
+add = lambda x, y: x + y
+print(add(2, 5))
